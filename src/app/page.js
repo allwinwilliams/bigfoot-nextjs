59281d0153fb7472ac8d86689df2395b5aad54f4
Allwin Williams <allwinwilliams.info@gmail.com>
2024-08-01 11:16:54 +0530

    Change Video Backgrounds

--- src/app/page.js
+++ src/app/page.js
@@ -39,7 +39,7 @@ export default function HomePage() {
             Your browser does not support the video tag.
           </video>
           <Paper elevation={4} sx={{
-            padding: 8,
+            padding: {md: 8, xs: 4},
             borderRadius: 4,
             mt: 2,
             boxShadow: '0 0 24px rgba(0, 0, 0, 0.12)',
@@ -49,7 +49,7 @@ export default function HomePage() {
             backgroundColor: 'rgba(0, 0, 0, 0.4)',
             zIndex: 1,
           }}>
-            <Grid container spacing={4} alignItems="center" justifyContent="center" sx={{ paddingTop: 20 }}>
+            <Grid container spacing={4} alignItems="center" justifyContent="center" sx={{ paddingTop: {md: 20, xs: 10} }}>
               <Grid item xs={12} md={12} sx={{ display: 'flex', justifyContent: 'center' }}>
                 <img
                   src="/song-tshirt/customised-banner.png"
@@ -59,7 +59,7 @@ export default function HomePage() {
               </Grid>
               <Grid item xs={12} md={8}>
                 <Typography
-                  variant="h3"
+                  variant="h4"
                   sx={{
                     fontWeight: 'bold',
                     mb: 2,
@@ -103,87 +103,7 @@ export default function HomePage() {
 
 
 
-        <Box sx={{ position: 'relative', width: '100%', backgroundColor: '#000000', borderRadius: 8 }}>
-          <video
-            autoPlay
-            loop
-            muted
-            style={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: '100%',
-              objectFit: 'cover',
-              borderRadius: 16,
-              zIndex: 0,
-            }}
-          >
-            <source src="/gradient-background.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-          <Paper elevation={4} sx={{
-            padding: 4,
-            borderRadius: 16,
-            mt: 4,
-            boxShadow: '0 0 24px rgba(0, 0, 0, 0.12)',
-            color: 'white',
-            position: 'relative',
-            overflow: 'hidden',
-            backgroundColor: 'rgba(0, 0, 0, 0.5)',
-            zIndex: 1,
-          }}>
-            <Grid container spacing={4} alignItems="center" style={{zIndex: 999}}>
-              <Grid item xs={12} md={6}>
-                <img
-                  src="/song-tshirt/customised-banner.png"
-                  alt="Custom T-shirt"
-                  style={{ width: '100%', borderRadius: '16px' }}
-                />
-              </Grid>
-              <Grid item xs={12} md={6}
-              sx={{paddingX: 2}}>
-                <Typography
-                  variant="h3"
-                  sx={{
-                    fontWeight: 'bold',
-                    mb: 2,
-                  }}
-                >
-                  Customise your <nobr>T-shirt</nobr> with songs
-                </Typography>
-                <Typography variant="subtitle2"
-                sx={{
-                  color: 'rgba(255, 255, 255, 0.8)'
-                }}>
-                  Imagine wearing your favorite song! Now you can customize your T-shirt with lyrics, album art, or any design that represents the music you love. Our high-quality printing ensures that your custom T-shirt looks amazing and lasts long. Create a unique piece of clothing that speaks to your musical tastes and personality.
-                </Typography>
-                
-                <Button 
-                variant="contained" 
-                color="primary" 
-                component={Link} 
-                href="/product/songtshirt" 
-                startIcon={<CreateIcon />}
-                sx={{ 
-                  mt: 4, 
-                  padding: 2, 
-                  fontWeight: 'bold', 
-                  fontFamily: 'Inter', 
-                  textTransform: 'none',
-                  background: 'linear-gradient(45deg, #fd1f4f 20%, #FF8E53 80%)',
-                  backgroundSize: '400% 400%',
-                  animation: 'backgroundMovement 4s ease infinite',  
-                  borderRadius: 4,
-                  width: '100%'
-                }} 
-              >
-                Craft Your Own Tshirt
-              </Button>
-              </Grid>
-            </Grid>
-          </Paper>
-        </Box>      
+          
 
         </Container>
         <AutoScrollCards />
@@ -300,6 +220,169 @@ export default function HomePage() {
       </Box>
 
       <Container>
+      <Box sx={{ position: 'relative', width: '100%', backgroundColor: '#000000', borderRadius: 8 }}>
+        <video
+          autoPlay
+          loop
+          muted
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            objectFit: 'cover',
+            borderRadius: 16,
+            zIndex: 0,
+          }}
+        >
+          <source src="https://videos.pexels.com/video-files/4669695/4669695-uhd_2732_1440_25fps.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <Paper elevation={4} sx={{
+          padding: 4,
+          borderRadius: 4,
+          mt: 4,
+          boxShadow: '0 0 24px rgba(0, 0, 0, 0.12)',
+          color: 'white',
+          position: 'relative',
+          overflow: 'hidden',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          zIndex: 1,
+        }}>
+          <Grid container spacing={4} alignItems="center" style={{zIndex: 999}}>
+            <Grid item xs={12} md={6}>
+              <img
+                src="/song-tshirt/customised-banner.png"
+                alt="Custom T-shirt"
+                style={{ width: '100%', borderRadius: '16px' }}
+              />
+            </Grid>
+            <Grid item xs={12} md={6}
+            sx={{paddingX: 2}}>
+              <Typography
+                variant="h4"
+                sx={{
+                  fontWeight: 'bold',
+                  mb: 2,
+                }}
+              >
+                Customise with Music.< br/>
+                Choose from millions.
+              </Typography>
+              <Typography variant="subtitle2"
+              sx={{
+                color: 'rgba(255, 255, 255, 0.8)'
+              }}>
+                Imagine wearing your favorite song! Now you can customize your T-shirt with lyrics, album art, or any design that represents the music you love. Our high-quality printing ensures that your custom T-shirt looks amazing and lasts long. Create a unique piece of clothing that speaks to your musical tastes and personality.
+              </Typography>
+              
+              <Button 
+              variant="contained" 
+              color="primary" 
+              component={Link} 
+              href="/product/songtshirt" 
+              startIcon={<CreateIcon />}
+              sx={{ 
+                mt: 4, 
+                padding: 2, 
+                fontWeight: 'bold', 
+                fontFamily: 'Inter', 
+                textTransform: 'none',
+                background: 'linear-gradient(45deg, #AD26FF 20%, #FF26CF 80%)',
+                backgroundSize: '400% 400%',
+                animation: 'backgroundMovement 4s ease infinite',  
+                borderRadius: 4,
+                width: '100%'
+              }} 
+            >
+              Craft with Music
+            </Button>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Box>    
+
+      <Box sx={{ position: 'relative', width: '100%', backgroundColor: '#000000', borderRadius: 8 }}>
+        <video
+          autoPlay
+          loop
+          muted
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            objectFit: 'cover',
+            borderRadius: 16,
+            zIndex: 0,
+          }}
+        >
+          <source src="https://videos.pexels.com/video-files/18069235/18069235-uhd_2560_1440_24fps.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <Paper elevation={4} sx={{
+          padding: 4,
+          paddingBottom: 1,
+          borderRadius: 4,
+          mt: 4,
+          boxShadow: '0 0 24px rgba(0, 0, 0, 0.12)',
+          // color: 'white',
+          position: 'relative',
+          overflow: 'hidden',
+          backgroundColor: 'rgba(255, 255, 255, 0.2)',
+          zIndex: 1,
+        }}>
+          <Grid container spacing={4} alignItems="center" style={{zIndex: 999}}>
+            <Grid item xs={12} md={6}
+            sx={{paddingX: 4}}>
+              <Typography
+                variant="h4"
+                sx={{
+                  fontWeight: 'bold',
+                  mb: 2,
+                }}
+              >
+                Draw with a prompt.<br/>
+                Be imaginative.
+              </Typography>
+              <Typography variant="subtitle2"
+              sx={{
+                color: 'rgba(0, 0, 0, 0.8)'
+              }}>
+                Imagine anything. Make it alive with a simple prompt.
+              </Typography>
+              
+              <Button 
+              variant="contained" 
+              color="primary" 
+              component={Link} 
+              href="/product/songtshirt" 
+              startIcon={<CreateIcon />}
+              sx={{ 
+                mt: 4, 
+                padding: 2, 
+                fontWeight: 'bold', 
+                fontFamily: 'Inter', 
+                textTransform: 'none',
+                borderRadius: 4,
+                width: '100%'
+              }} 
+            >
+              Start Imagining
+            </Button>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <img
+                src="/landing-page/ai-banner.png"
+                alt="Custom T-shirt"
+                style={{ width: '100%', borderRadius: '16px' }}
+              />
+            </Grid>
+          </Grid>
+        </Paper>
+      </Box>    
       
         <SongProductStaticContent/>
         <Paper elevation={4} sx={{ padding: 4, borderRadius: 4, mt: 4 }}>
@@ -319,19 +402,19 @@ export default function HomePage() {
                   mb: 2,
                 }}
               >
-                Customise your <nobr>T-shirt</nobr> with songs
+                Pick a song. Write a prompt. Click a button.
               </Typography>
               <Typography variant="subtitle2"
               sx={{
                 color: 'rgba(0, 0, 0, 0.8)'
               }}>
-                Imagine wearing your favorite song! Now you can customize your T-shirt with lyrics, album art, or any design that represents the music you love. Our high-quality printing ensures that your custom T-shirt looks amazing and lasts long. Create a unique piece of clothing that speaks to your musical tastes and personality.
+                Imagine wearing your favorite song! Now you can customize your T-shirt with lyrics, or any design that represents the music you love.
               </Typography>
               <Button 
                 variant="contained" 
                 color="primary" 
                 component={Link} 
-                href="/product" 
+                href="#the-range" 
                 startIcon={<CreateIcon />}
                 sx={{ 
                   mt: 4, 
