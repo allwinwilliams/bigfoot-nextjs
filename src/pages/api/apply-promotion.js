838398a77dd2c1a288f3261f514b868b6eb40104
Allwin Williams <allwinwilliams.info@gmail.com>
2024-09-16 16:23:26 +0530

    Woxsen promotion code

--- src/pages/api/apply-promotion.js
+++ src/pages/api/apply-promotion.js
@@ -51,9 +51,9 @@ export default async function handler(req, res) {
         reference_id: "WOXSEN_OFFER",
         type: "offer",
         code: "WOXSEN",
-        value: 60000,
+        value: 40000,
         value_type: "Fixed amount",
-        description: "Rs. 600 off for Woxsen"
+        description: "Rs. 400 off for Woxsen"
       },
       "FAM1000": {
         reference_id: "PROMO2",
